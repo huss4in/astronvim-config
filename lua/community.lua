b58b8f361dd591694caf287eeb43a50660bf672b
Hussain <huss4in7@outlook.com>
2024-05-23 10:38:00 +0300

    update(community): Add community plugins

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -9,4 +7,51 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+
+  -- Colorscheme
+  { import = "astrocommunity.colorscheme.catppuccin" },
+
+  -- Pack
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.markdown" },
+
+  -- Completions
+  { import = "astrocommunity.completion.copilot-lua" },
+  {
+    "copilot.lua",
+    opts = {
+      suggestion = {
+        keymap = {
+          accept = "<C-l>",
+          accept_word = "<C-k>",
+          accept_line = "<C-j>",
+          next = "<M-.>",
+          prev = "<M-,>",
+          dismiss = "<C-c>",
+        },
+      },
+    },
+  },
+
+  -- Others
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = {
+      colorcolumn = "150",
+      -- disabled_filetypes = { "help", "text", "markdown" },
+      -- custom_colorcolumn = {},
+      -- scope = "file",
+    },
+  },
+
+  -- VSCode integration
+  { import = "astrocommunity.recipes.vscode" },
 }
