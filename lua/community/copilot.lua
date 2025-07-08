3847ba9cabf4dbb31f40d7037aa1dc45030c9f14
Hussmin <huss4in7@outlook.com>
2025-07-08 14:18:11 +0300

    update(Copilot)

--- lua/community/copilot.lua
+++ lua/community/copilot.lua
@@ -2,20 +2,5 @@
 return {
   "AstroNvim/astrocommunity",
 
-  { import = "astrocommunity.completion.copilot-lua" }, -- lua/community/copilot.lua
-  {
-    "copilot.lua",
-    opts = {
-      suggestion = {
-        keymap = {
-          accept = "<C-l>",
-          accept_word = "<C-k>",
-          accept_line = "<C-j>",
-          next = "<M-.>",
-          prev = "<M-,>",
-          dismiss = "<C-c>",
-        },
-      },
-    },
-  },
+  { import = "astrocommunity.completion.copilot-lua-cmp" }, -- lua/community/copilot.lua
 }
